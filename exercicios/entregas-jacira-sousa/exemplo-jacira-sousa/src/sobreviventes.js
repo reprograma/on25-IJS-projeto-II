956ca5eed82fcf18f4652daab5ded7778011f3e5
guacirita <guacirita@gmail.com>
2023-09-02 11:52:45 -0300

    iniciando o projetoII

---
+++ exercicios/entregas-jacira-sousa/exemplo-jacira-sousa/src/sobreviventes.js
@@ -0,0 +1,21 @@
+
+class Sobreviventes {
+  nome;
+  ferimentos = 0;
+
+  constructor(nome) {
+    this.nome = nome;
+    this.ferimentos = 0;
+  }
+
+
+}
+
+
+
+
+
+
+
+
+module.exports = Sobreviventes;
