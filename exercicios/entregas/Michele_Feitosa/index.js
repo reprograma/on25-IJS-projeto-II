f78521810874caaabfa9b15a7666fa069f0c1420
michelefeitosa <michelefeitosa08@gmail.com>
2023-09-02 11:53:08 -0300

    classe sobreviventes

---
+++ exercicios/entregas/Michele_Feitosa/index.js
@@ -0,0 +1,14 @@
+
+class Sobreviventes{
+ nome;
+
+ constructor(nome){
+    this.nome=nome;
+    this.ferimentos = 0;
+ }
+
+ //Se um sobrevivente sofre 3 ferimentos, ele morre imediatamente;
+
+
+
+}
