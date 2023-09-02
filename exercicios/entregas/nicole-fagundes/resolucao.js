758c9e2e4802da57885e48890b46f905c0129e34
Nicole Siqueira Fagundes <psi.fagundes@gmail.com>
2023-09-02 11:50:24 -0300

    feat: iniciando o projeto

---
+++ exercicios/entregas/nicole-fagundes/resolucao.js
@@ -0,0 +1,17 @@
+// Sobreviventes
+
+// 1. Criar classe de Sobreviventes
+
+class Survivor {
+    name;
+    wound = 0;
+    // árvore de habilidades
+
+    constructor(name){
+        this.name = name;
+    }
+
+    // wounds(){
+    //     if()
+    // }
+}
