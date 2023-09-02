e6229bfb5cce4d4d6037dec79b04ed5acfc38f02
DoriansFer <doohfernandes@yahoo.com>
2023-09-02 15:08:40 -0300

    equipments

--- exercicios/entregas/src/equipment.js
+++ exercicios/entregas/src/equipment.js
@@ -3,4 +3,18 @@ class Equipamento {
         this.nome = nome;
         this.categoria = categoria;
     }
+
+    atualizarCapacidadeEquipamentos() {
+        const capacidadeMaxima = 7 - this.ferimentos;
+        if (this.equipamentosMaos.length > 2) {
+            this.equipamentosReserva.push(this.equipamentosMaos.pop());
+        }
+        while (this.equipamentosMaos.length > 2) {
+            this.equipamentosReserva.push(this.equipamentosMaos.pop());
+        }
+        while (this.equipamentosReserva.length > capacidadeMaxima - this.equipamentosMaos.length) {
+            // O sobrevivente precisa descartar equipamentos de reserva
+            // Vou implementar a lógica para escolher qual equipamento descartar
+        }
+    }
 }
