9184ff6301b39cd34aa4a8a4ad30a767637dbf47
elvirabl <elvirabrunoleme@gmail.com>
2023-09-02 11:54:01 -0300

    Projeto II - Aula - IJS - On25 {Reprograma}

---
+++ exercicios/entregas/ElviraBruno/resolucao.js
@@ -0,0 +1,18 @@
+// Definição do objeto Sobrevivente
+class Sobrevivente {
+    constructor(nome) {
+      this.nome = nome;
+      this.ferimentos = 0;
+      this.acoes = 3;
+      this.equipamentos = {
+        maos: [],
+        reserva: [],
+      };
+      this.nivel = "Azul";
+      this.experiencia = 0;
+      this.arvoreHabilidades = [];
+    }
+  
+  }
+  
+  
