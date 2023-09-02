aaac5700701c6c9519a7e1a4366df1a1d2d4f249
elvirabl <elvirabrunoleme@gmail.com>
2023-09-02 16:28:40 -0300

    Elvira Bruno - Projeto II - Aula/tarde S11 - IJS - On25 {Reprograma}

--- exercicios/entregas/ElviraBruno/teste1.js
+++ exercicios/entregas/ElviraBruno/teste1.js
@@ -10,6 +10,7 @@ class Sobrevivente {
     this.arvoreHabilidades = [];
   }
 
+  // Método para adicionar um equipamento (mãos ou mochila) do sobrevivente
   adicionarEquipamento(equipamento) {
     if (this.equipamentosMaos.length < 2) {
       this.equipamentosMaos.push(equipamento);
@@ -22,6 +23,7 @@ class Sobrevivente {
     }
   }
 
+      // Método para remover um equipamento das mãos do sobrevivente
       removerEquipamentoMaos(equipamento) {
         const index = this.equipamentosMaos.indexOf(equipamento);
         if (index !== -1) {
@@ -31,6 +33,7 @@ class Sobrevivente {
         }
       }
     
+      // Método para remover um equipamento da Mochila do sobrevivente
       removerEquipamentoMochila(equipamento) {
         const index = this.equipamentosMochila.indexOf(equipamento);
         if (index !== -1) {
@@ -39,10 +42,13 @@ class Sobrevivente {
           console.log(`Amigo '${this.nome}' Não encontrou o equipamento na mochila.`);
         }
       }
+  
+
+  // Método para lidar com ferimentos
   ferir(dano) {
     if (this.ferimentos < 3) {
       this.ferimentos += dano;
-      const equipamentosPerdidos = this.ferimentos;
+      const equipamentosPerdidos = this.ferimentos; // Quantidade de equipamentos perdidos
       this.capacidadeCarregamento = this.equipamentosMaos.length + this.equipamentosMochila.length - equipamentosPerdidos;
 
       if (this.capacidadeCarregamento < 0) {
@@ -57,4 +63,26 @@ class Sobrevivente {
     }
   }
 }
+// Exemplo de uso da classe Sobrevivente
+const sobrevivente1 = new Sobrevivente("Soldado Ryan");
+
+sobrevivente1.ferir(1); // Causa 1 ferimento
+//sobrevivente1.ferir(1); // Causa 1 ferimento
+//sobrevivente1.ferir(1); // Causa 1 ferimento, o sobrevivente morre
+
+console.log(
+  `Ferimentos de '${sobrevivente1.nome}': ${sobrevivente1.ferimentos}`
+);
+sobrevivente1.adicionarEquipamento("bastão de baseball"); // Adiciona à mão
+sobrevivente1.adicionarEquipamento("frigideira"); // Adiciona à mão
+sobrevivente1.adicionarEquipamento("kit de primeiros socorros"); // Adiciona à mochila
+sobrevivente1.adicionarEquipamento("Garrafa de água"); // Adiciona à mochila
+
+console.log(`Equipamentos nas mãos de '${sobrevivente1.nome}': ${sobrevivente1.equipamentosMaos}`);
+sobrevivente1.removerEquipamentoMaos("frigideira"); // Remove um equipamento das mãos
+
+console.log(`Equipamentos nas mãos de '${sobrevivente1.nome}' após remover: ${sobrevivente1.equipamentosMaos}`);
+
+console.log(`Equipamentos nas mãos de '${sobrevivente1.nome}': ${sobrevivente1.equipamentosMaos}`); // Exibe os equipamentos nas mãos
+console.log(`Equipamentos na mochila de '${sobrevivente1.nome}': ${sobrevivente1.equipamentosMochila}`); // Exibe os equipamentos na mochila
 
