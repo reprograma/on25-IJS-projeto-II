e8dfa58e0c57e99226976daf5e8ad2c0633a9de0
brena-odwyer <odwyer.brena@gmail.com>
2023-09-07 16:59:14 -0400

    feat: class Gear

--- exercicios/entregas/brena-odywer/index.js
+++ exercicios/entregas/brena-odywer/index.js
@@ -1,6 +1,24 @@
-const { Survivor } = require("./resolucao")
+const { Survivor } = require("./Survivor")
 // const { SkillTree } = require("./resolucao")
-// const { Gear } = require("./resolucao")
+const { Gear } = require("./Gear")
+
+
 
 let brena = new Survivor ("brena")
+console.log(Survivor.allSurvivors)
+let raquel = new Survivor("raquel")
+console.log(Survivor.allSurvivors)
+console.log("--------------")
 console.log(brena.isAlive())
+console.log("--------------")
+console.log(brena.isHarmed())
+console.log(brena.isHarmed())
+console.log(brena.isHarmed())
+console.log("--------------")
+console.log(brena.isAlive())
+console.log(Survivor.allSurvivors)
+console.log("--------------")
+let brenaGear = new Gear
+brenaGear.addGear('baseball bat')
+console.log(brenaGear.gear)
+console.log("--------------")
