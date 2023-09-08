30407b510269709cfe244693d584671655fa198f
hinale <leticiaamarallara@hotmail.com>
2023-09-08 19:41:35 -0300

    adiciona testes da classe Survivor

--- exercicios/entregas/leticia-amaral/entities/SkillsThree.js
+++ exercicios/entregas/leticia-amaral/entities/SkillsThree.js
@@ -1,9 +1,20 @@
 const Levels = require("../constants/levels");
-const Skills = require("../constants/skills");
 
 class SkillsThree {
   level = Levels.BLUE;
   allSkills = [];
+
+  checkLevelUp(experience) {
+    if(experience >= Levels.UPGRADE_ORANGE) {
+      this.level = Levels.RED;
+    } else if (experience >= Levels.UPAGRADE_YELLOW) {
+      this.level = Levels.ORANGE;
+    } else if (experience >= Levels.UPGRADE_BLUE) {
+      this.level = Levels.YELLOW;
+    } else {
+      this.level = Levels.BLUE;
+    }
+  }
 }
 
 module.exports = SkillsThree;
