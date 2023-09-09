12e6595996c733723bf6ebbbf2791034c370a147
Ciane <100417980+ciane-pereira@users.noreply.github.com>
2023-09-08 23:17:57 -0300

    Projeto II Zombicide

---
+++ exercicios/entregas/ciane-pereira/src/Skill.js
@@ -0,0 +1,35 @@
+const { Level } = require('./Level')
+
+class Skill {
+    skillTree;
+    potencialSkills = [
+        '+1 Dano',
+        'Tesouro escondido',
+        '+1 Ação de Movimento',
+        '+1 equipamento em mãos',
+        '+1 vida'
+    ];
+    unlockedSkills = ['+1 Ação'];
+
+    abilities() {
+        switch (this.level) {
+            case 'Yellow':
+                this.skillTree = 1;
+                break;
+            case 'Orange':
+                this.skillTree = 2;
+                break;
+            case 'Red':
+                this.skillTree = 3;
+                break;
+        }
+    }
+
+    unlockedSkills() {
+        if (this.level === 'Yellow') {
+            this.skillTree.push(unlockedSkills[0]);
+        }
+    }
+}
+
+module.exports = { Skill }
